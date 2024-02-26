9b08940fbe6367528533622fcc1f9e719a18c3d5
Rain徐正茂 <rain.xu@galaxyoversea.com>
2024-02-26 10:28:11 +0800

    feat: 升级command命令脚本

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,24 @@
 #! /usr/bin/env node
-console.log('hello world') 
+
+const { program } = require('commander');
+
+program.version('1.0.0') // -v 或者 --versions输出版本号
+
+program
+  .command('init <template> <project>')
+  .description('初始化项目模版')
+  .action((templateName, projectName) => {
+    console.log(templateName, projectName)
+  })
+
+program
+  .command('list')
+  .description('查看所有可用的模版')
+  .action(() => {
+    console.log(
+      `template-vue vue模板
+      template-react react模板`\
+    )
+  })
+
+program.parse(process.argv);
